8e878780afed06e06fc153cb101902fd5ca8f591
NotCampeur <lucas.dutriez@gmail.com>
2020-12-03 17:08:28 +0100

    Adding date to log files and colors to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+PURPLE="\e[35m"
+DEFAULT="\e[0m"
+
 services="nginx wordpress"
 # services="wordpress"
 
@@ -7,17 +12,28 @@ ft_build_image()
 {
 	for service in $services
 	do
-		docker build srcs/$service -t $service > .log/$service.log
+		echo -en ${YELLOW}"\tBuilding $service..."${DEFAULT}
+		echo -n "docker build srcs/$service -t $service -" > .log/$service.log
+		date >> .log/$service.log
+		docker build srcs/$service -t $service >> .log/$service.log
+		echo -e ${GREEN}"DONE"${DEFAULT}
 	done
 }
 
 ft_run_container()
 {
+	echo -en ${YELLOW}"\tApplying metallb-deployment.yaml..."${DEFAULT}
+	echo -n "kubectl apply -f srcs/load_balancer/metallb-deployment.yaml -" >> .log/setup.log
+	date >> .log/setup.log
+	kubectl apply -f srcs/load_balancer/metallb-deployment.yaml >> .log/setup.log
+	echo -e ${GREEN}"DONE"${DEFAULT}
 	for service in $services
 	do
-		kubectl apply -f srcs/$service/$service-deployment.yaml
-		# kubectl create deployment srcs/$service/$service-deployment.yaml srcs/$service
-		# docker run -t $service >> .log/$service.log
+		echo -en ${YELLOW}"\tApplying $service-deployment.yaml..."${DEFAULT}
+		echo -n "kubectl apply -f srcs/$service/$service-deployment.yaml -" >> .log/setup.log
+		date >> .log/setup.log
+		kubectl apply -f srcs/$service/$service-deployment.yaml >> .log/setup.log
+		echo -e ${GREEN}"DONE"${DEFAULT}
 	done
 }
 
@@ -25,27 +41,38 @@ ft_start_minikube()
 {
 	if [[ $(minikube status | grep -c "Running") == 0 ]]
 	then
-		minikube start --driver=docker --extra-config=apiserver.service-node-port-range=1-35000
+		echo -e ${YELLOW}"\tInstalling minikube and metallb..."${DEFAULT}
+		echo -e ${PURPLE}; minikube start --driver=docker --extra-config=apiserver.service-node-port-range=1-35000
 		minikube addons enable metrics-server
 		minikube addons enable dashboard
 		kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 		kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 		kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" > /dev/null
 	else
-		kubectl delete pods --all
-		kubectl delete deployments --all
-		kubectl delete svc --all
-		kubectl delete pvc --all
+		echo -en ${YELLOW}"\tCleaning the previous minikube container..."${DEFAULT}
+		echo -n "kubectl delete pods --all -" > .log/setup.log ; date >> .log/setup.log
+		kubectl delete pods --all >> .log/setup.log
+		echo -n "kubectl delete deployments --all -" >> .log/setup.log ; date >> .log/setup.log
+		kubectl delete deployments --all >> .log/setup.log
+		echo -n "kubectl delete svc --all -" >> .log/setup.log ; date >> .log/setup.log
+		kubectl delete svc --all >> .log/setup.log
+		echo -n "kubectl delete pvc --all -" >> .log/setup.log ; date >> .log/setup.log
+		kubectl delete pvc --all >> .log/setup.log
+		echo -e ${GREEN}"DONE"${DEFAULT}
 	fi
 	eval $(minikube docker-env)
 }
 
-# minikube start --driver=docker --cpus=2 --memory=2200 --extra-config=apiserver.service-node-port-range=1-35000
-
 yes | docker system prune > /dev/null
+
+echo -e ${PURPLE}"[ Minikube | Metallb ]"${DEFAULT}
 ft_start_minikube
+
+echo -e ${PURPLE}"[ Docker images building ]"${DEFAULT}
 ft_build_image
-kubectl apply -f srcs/load_balancer/metallb-deployment.yaml
+
+echo -e ${PURPLE}"[ YAML Kubernetes applying ]"${DEFAULT}
 ft_run_container
 
+echo -e ${PURPLE}"[ Finished ]"
 # minikube dashboard
